c65b80d8fd19ce04927a145627e7f72b339484b8
Mahmoud Darwish <imahmwd14@gmail.com>
2020-11-01 15:56:41 +0700

    initial commit

--- src/superDigit.kt
+++ src/superDigit.kt
@@ -1,5 +1,8 @@
 fun superDigit(n: String, k: Int): Int {
-    if (k > -1) return superDigit((n.map { it.toString().toLong() }.sum() * k).toString(), -1)
-    else if (n.length == 1) return n.toInt()
+    if (k > -1)
+        return superDigit((n.map { it.toString().toLong() }.sum() * k).toString(), -1)
+    else if (n.length == 1)
+        return n.toInt()
+
     return superDigit(n.map { it.toString().toLong() }.sum().toString(), k)
 }
